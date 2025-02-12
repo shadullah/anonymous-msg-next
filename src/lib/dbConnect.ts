07f93b31fb43c4582c59b3ff62cf8c782b951df9
hexaalif <shadullahsad9@gmail.com>
2025-02-12 21:59:42 +0600

    nextapp configuring

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+type ConnectObject= {
+    isConnected?: number 
+}
+
+const connection:ConnectObject= {}
+
+async function dbConnect():Promise<void> {
+    if(connection.isConnected){
+        console.log("already connected to database");
+        return 
+    }
+
+    try{
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
+        console.log(db);
+        console.log(db.connection);
+
+        connection.isConnected= db.connections[0].readyState
+
+        console.log("DB connected successfully!!");
+    }
+    catch(error){
+        console.log("database connection failed ", error);
+        process.exit()
+    }
+}
+
+export default dbConnect;
---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const messageSchema = z.object({
+    content:z.string().min(10, {message:"content must be at least 10 characters"})
+    .max(300, {message:"content must be no longer more than 300 characters"})
+})
